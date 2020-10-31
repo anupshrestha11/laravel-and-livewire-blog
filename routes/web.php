10a61b3e97268d1efd3acd479189594893ffad87
Anup-Shrestha <anupshresth9@gmail.com>
2020-10-31 22:08:51 +0545

    resource route set

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,37 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+// * dashboard route only
+Route::prefix("dashboard")->group(function () {
+    Route::get('/', function () {
+        return view('welcome');
+    })->name('dashboard');
+    Route::resource('posts', PostController::class, ['as' => 'dashboard']);
+});
+
+// * upload ckeditor image
+Route::post('/upload', function (Request $request) {
+
+    if ($request->hasFile('upload')) {
+        $orgName   = $request->file('upload')->getClientOriginalName();
+        $fileName  = pathinfo($orgName, PATHINFO_FILENAME);
+        $extension = $request->file('upload')->getClientOriginalExtension();
+        $fileName  = $fileName . '_' . time() . '.' . $extension;
+
+        $request->file('upload')->move(public_path('images'), $fileName);
+
+        $CKEditorFuncNum = $request->input('CKEditorFuncNum');
+        $url             = asset('images/' . $fileName);
+        $msg             = 'Image Uploaded Successfully';
+        $response        = "
+        <script>
+        window.parent.CKEDITOR.tools.callFunction($CKEditorFuncNum,'$url', '$msg')
+        </script>
+        ";
+
+        @header('Content-type: text/html; charset=utf-8');
+        echo $response;
+    }
+
+})->name('upload');
